fa18e29d306aec87773b79147051cdc6086e7696
MohamedNawfal-7 <mohamednawfal183@gmail.com>
2024-08-22 17:22:25 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-function Userdemo(){
-  const[users,setUsers]=useState([]);
-  const[loading,setLoading]=useState(true);
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data=>{
-        setUsers(data);
-        setLoading(false);
-      })
-      .catch(error=>{
-        console.error("If it is not loading, will be an error message")
-        setLoading(false);
-      })
-  },[])
-  if(loading){
-    return<p>Loading...</p>;
-  }
+import Shoppingcart from './Shoppingcart'
+function Mydemo(){
   return(
-    <div>
-      <h1>List out the users in API</h1>
-      <ul>
-        {users.map(users=>(
-          <li key={users.id}>{users.name}</li>
-        ))}
-      </ul>
-    </div>
+  <div>
+    <Shoppingcart/>
+  </div>
   );
 }
-ReactDOM.render(<Userdemo/>,document.getElementById("root"));
+
+ReactDOM.render(<Mydemo/>,document.getElementById("root"));
